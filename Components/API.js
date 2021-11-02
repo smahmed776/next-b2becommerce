c86a2ed3e6412c906bc05d8c477d94bb41f1a57f
Syed Mahbub <xsmahmed776@gmail.com>
2021-11-02 23:15:28 +0600

    api fix

--- Components/API.js
+++ Components/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api/auth/v1/",
+    baseURL: "/api/auth/v1/",
     withCredentials: true,
     credentials: "include"
 })
